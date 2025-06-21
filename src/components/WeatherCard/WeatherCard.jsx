e80f4a9094269b9ca8f23443f7bbcfcd07bf0374
Donovan Jabbar <djabbar51@gmail.com>
2025-06-21 11:08:48 -0600

    refactor: add optional chaining to dynamic image url

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import './WeatherCard.css'
 
 const WeatherCard = ({ temperature, condition, isDay }) => {
-  const imageUrl = new URL(`../../assets/images/cards/${condition.toLowerCase()}_${isDay ? "day" : "night"}.png`, import.meta.url)
+  const imageUrl = new URL(`../../assets/images/cards/${condition?.toLowerCase()}_${isDay ? "day" : "night"}.png`, import.meta.url)
 
   return (
     <div className='weather-card'>
-      <p className='weather-card__temperature'>{`${temperature}`}°F</p>
+      <h1 className='weather-card__temperature'>{temperature}°F</h1>
       <img src={imageUrl} alt="weather card background" className='weather-card__background'/>
     </div>
   )
