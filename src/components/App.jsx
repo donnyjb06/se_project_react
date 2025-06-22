dc8397c79f60eb376cebe6cd542b25e833f51473
Donovan Jabbar <djabbar51@gmail.com>
2025-06-22 09:47:53 -0600

    feat: implement reusable form modal component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,19 @@ import { fetchWeatherData } from '../utils/weatherApi';
 import { LOCATION } from '../utils/constants';
 import { useEffect, useState } from 'react';
 import Main from './Main/Main';
+import ModalWithForm from './ModalWithForm/ModalWithForm';
 
 function App() {
   const [data, setData] = useState();
+  const [name, setName] = useState();
+  const [link, setLink] = useState();
+  const [tempRange, setTempRange] = useState();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
     const setWeatherData = () => {
       fetchWeatherData(LOCATION)
         .then((res) => {
-          console.log(res);
           const utcDate = new Date(res?.dt * 1000);
           const localTime = new Date(utcDate.getTime() + res?.timezone * 1000);
           const hour = localTime.getHours();
@@ -30,14 +34,104 @@ function App() {
     setWeatherData();
   }, []);
 
+  const onClose = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <>
+      <button onClick={() => setModalIsOpen(true)}>open modal</button>
       <Header city={data?.city} />
       <Main
         temperature={data?.temperature}
         isDay={data?.isDay}
         condition={data?.condition}
       />
+      {modalIsOpen && (
+        <ModalWithForm
+          title='New garment'
+          btnLabel='Add garment'
+          formId='add-garment-form'
+          onClose={onClose}>
+          <label className='form__label form__label_for_text'>
+            Name
+            <input
+              type='url'
+              className='form__input'
+              name='name'
+              placeholder='Name'
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </label>
+          <label className='form__label form__label_for_text'>
+            Image
+            <input
+              type='text'
+              className='form__input'
+              name='link'
+              placeholder='Image URL'
+              onChange={(e) => setLink(e.target.value)}
+              required
+            />
+          </label>
+          <fieldset className='form__fieldset'>
+            <legend className='form__legend'>Select the weather type:</legend>
+
+            <div className='form__group'>
+              <input
+                type='radio'
+                value='hot'
+                className='form__input form__input_type_radio'
+                checked={tempRange === 'hot'}
+                name='weather'
+                onChange={(e) => setTempRange(e.target.value)}
+                id='weather-hot'
+                required
+              />
+              <label
+                className='form__label form__label_for_weather'
+                htmlFor='weather-hot'>
+                Hot
+              </label>
+            </div>
+
+            <div className='form__group'>
+              <input
+                type='radio'
+                value='warm'
+                className='form__input form__input_type_radio'
+                checked={tempRange === 'warm'}
+                name='weather'
+                onChange={(e) => setTempRange(e.target.value)}
+                id='weather-warm'
+              />
+              <label
+                className='form__label form__label_for_weather'
+                htmlFor='weather-warm'>
+                Warm
+              </label>
+            </div>
+
+            <div className='form__group'>
+              <input
+                type='radio'
+                value='cold'
+                className='form__input form__input_type_radio'
+                checked={tempRange === 'cold'}
+                name='weather'
+                onChange={(e) => setTempRange(e.target.value)}
+                id='weather-cold'
+              />
+              <label
+                className='form__label form__label_for_weather'
+                htmlFor='weather-cold'>
+                Cold
+              </label>
+            </div>
+          </fieldset>
+        </ModalWithForm>
+      )}
     </>
   );
 }
---
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import exitIcon from "../../assets/images/exit-icon-light.svg"
+import './ModalWithForm.css'
+
+const ModalWithForm = ({ title, btnLabel, formId, children, onClose }) => {
+
+  useEffect(() => {
+    const handleEscapeClose = (event) => {
+      if (event.key === "Escape") {
+        onClose()
+      }
+    }
+
+    document.addEventListener("keydown", handleEscapeClose)
+
+    return (() => {
+      document.removeEventListener("keydown", handleEscapeClose)
+    })
+  })
+
+  return (
+    <div className={`modal modal_type_${formId}`} role='dialog' aria-modal="true" onClick={(event) => {
+      if (event.target.classList.contains('modal')) {
+        onClose();
+      }
+    }}>
+      <div className='modal__container'>
+        <button className='modal__exit' onClick={onClose}>
+          <img src={exitIcon} alt="x icon for exit button" className='modal__exit-icon'/>
+        </button>
+        <h2 className='modal__title'>{title}</h2>
+        <form className='form' id={formId} name={formId}>
+          {children}
+          <button className='form__submit'>{btnLabel}</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWithForm;
