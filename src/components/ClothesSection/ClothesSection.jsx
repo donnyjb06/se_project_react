9ebfcc063f0cb03187e6efc88b38b68f7324ce62
Donovan Jabbar <djabbar51@gmail.com>
2025-06-27 15:35:52 -0600

    feat: style clothing gallery in profile route

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import './ClothesSection.css';
+import { defaultClothingItems } from '../../utils/constants';
+import ItemCard from '../ItemCard/ItemCard';
 
 const ClothesSection = () => {
   return (
-    <div>ClothesSection</div>
+    <div className='clothes'>
+      <div className="clothes__header">
+        <h2 className="clothes__title">Your items</h2>
+        <button className="clothes__add-new">+ Add new</button>
+      </div>
+
+      <ul className="clothes__gallery">
+        {defaultClothingItems.map(item => (
+          <ItemCard
+            key={item._id}
+            link={item.link}
+            name={item.name}
+            condition={item.condition} />
+        ))}
+      </ul>
+    </div>
   )
 }
 
