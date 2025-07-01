38d5d50eed14906f1f7d12ead0d4d4ce2f3c0f9b
Donovan Jabbar <djabbar51@gmail.com>
2025-07-01 16:42:35 -0600

    refactor: remove unused import statements

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import exitIcon from "../../assets/images/exit-icon-light.svg"
 import './ModalWithForm.css'
 
--- src/components/ToggleSwitch/ToggleSwith.jsx
+++ src/components/ToggleSwitch/ToggleSwith.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './ToggleSwitch.css';
 
 const ToggleSwitch = ({ onToggle, optionLeft, optionRight, className = '' }) => {
