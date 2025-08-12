d5cfa81bfafaf13b04006c019c7348749d237924
Donovan Jabbar <djabbar51@gmail.com>
2025-08-12 13:54:04 -0500

    feat: add update profile and logout buttons to desktop view of sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,28 @@
 import profilePicture from '../../assets/images/temp-profile-picture.png';
 import './Sidebar.css';
 import { useUserData } from '../../hooks/useUserData';
+import { useModal } from '../../hooks/useModal';
 
 const Sidebar = () => {
   const { userData } = useUserData();
+  const { setModal } = useModal();
+  const { handleLogout } = useUserData();
   return (
     <div className='sidebar'>
-      <img
-        src={userData.avatar}
-        alt='Profile picture of Terrence Tegengne'
-        className='sidebar__profile-picture'
-      />
+      <div className='sidebar__user-details'>
+        <img
+          src={userData.avatar}
+          alt='Profile picture of Terrence Tegengne'
+          className='sidebar__profile-picture'
+        />
+        <h1 className='sidebar__name'>{userData.name}</h1>
+      </div>
 
       <div className='sidebar__controls'>
-        <h1 className='sidebar__name'>{userData.name}</h1>
-        <button className='sidebar__button sidebar__button_edit-profile'>
+        <button className='sidebar__button sidebar__button_edit-profile' onClick={() => setModal("update-profile-modal")}>
           Change profile data
         </button>
-        <button className='sidebar__button sidebar__button_log-out'>
+        <button className='sidebar__button sidebar__button_log-out' onClick={handleLogout}>
           Log out
         </button>
       </div>
