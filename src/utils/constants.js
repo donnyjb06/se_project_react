fc898ccba97814737efaabfe8c4ce6efb4dc9f55
Donovan Jabbar <djabbar51@gmail.com>
2025-08-12 17:59:45 -0500

    refactor: update coordinates constant variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,8 +3,8 @@ const API_KEY = '423225afe75e918fc11f1b4c26ddfac1';
 const BASE_URL = 'http://localhost:3001';
 
 const LOCATION = {
-  latitude: '40.514172',
-  longitude: '-112.033043',
+  latitude: '30.3462371',
+  longitude: '-94.1875574',
 };
 
 export { API_KEY, LOCATION, BASE_URL };
